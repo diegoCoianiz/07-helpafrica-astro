6a99bd1e52354c78533ab4cb4e8504cc7cf7c235
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-01-27 16:18:02 +0100

    feat: restructure files and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // astro integrations imports
 import image from "@astrojs/image";
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-
-
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://helpafrica.netlify.app',
   integrations: [
     mdx(),
     sitemap(),
-    tailwind({
-    config: {
-      applyBaseStyles: true,
-      path: './tailwind.config.cjs'
-    }
-    }),
+    tailwind(),
     react(),
-    image( {
-      logLevel: 'info',
-
-    } ),
+    image({
+      logLevel: "info",
+    }),
   ],
-
 });
